08091d4f0ff2deba6c812e6b346d82fd14943898
D4-80593-MeetVasani <w.vasanimeet@gmail.com>
2023-10-28 11:59:38 +0530

    hashing modified

--- Phase 1/hashing.cpp
+++ Phase 1/hashing.cpp
@@ -3,19 +3,44 @@ using namespace std;
 
 int main(){
 
-    int arr[10] = {0,1,1,2,2,2,3,4,4,5};
+
+    //---------------------hashing for arrays----------------------------
+    // int arr[10] = {0,1,1,2,2,2,3,4,4,5};
+
+    // //precompute
+    // int hashh[11] = {0};
+
+    // for(int i=0;i<10;i++){
+    //     hashh[arr[i]] += 1;
+    // }
+
+    // //fetch:
+    // int key;
+    // cout<<"Enter number you want to search : "<<endl;
+    // cin>>key;
+    // cout<< key << " has appeared " << hashh[key] <<" times in array ! " <<endl;
+    // return 0;
+
+
+    //-------------------hashing for string--------------------------------
+
+
+    string s = "abjsfdlaa";
 
     //precompute
-    int hashh[11] = {0};
+    int  hashh[26] = {0};
 
-    for(int i=0;i<10;i++){
-        hashh[arr[i]] += 1;
-    }
+    for(int i=0;i<s.size();i++){
+        hashh[s[i]-'a']++;
+    }   
 
-    //fetch:
-    int key;
-    cout<<"Enter number you want to search : "<<endl;
+    // fetch
+
+    char key;
+    cout<<"Enter which character you want to search : "<<endl;
     cin>>key;
-    cout<< key << " has appeared " << hashh[key] <<" times in array ! " <<endl;
-    return 0;
+
+    cout<< key <<" has occured " << hashh[key-'a'] << " times "<<endl;
+
+
 }
